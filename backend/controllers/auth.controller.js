8af93228e4305efdaac901f84b0756565bc5686c
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-07-16 11:09:30 +0100

    signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,34 @@
 import userModel from "../models/user.model.js"
-
+import bcrypt from "bcryptjs";
 
 
 const login = async (request, reply) => {
-    
     const users = await userModel.getAllUsers();
     console.log("------> users: ", users);
     return  {message: 'login'};    
 };
 
 const signup = async (request, reply) => {
-    return  {message: 'signup'};    
+    const {username, email, password} = request.body;
+
+    if (!username || !email || !password)
+        return reply.code(400).send({status: false, message: 'All fiels are required.'});
+        // throw new Error('');
+    const usernameAlreadyExist = userModel.getUserByUsername(username);
+    const emailAlreadyExist = userModel.getUserByUsername(email);
+
+    if (usernameAlreadyExist || emailAlreadyExist)
+        return reply.code(400).send({status: false, message: 'User already exists'});
+
+    try {
+        const hashedPassowrd = await bcrypt.hash(password, 10);
+        const user = userModel.createUser(username, email, hashedPassowrd);
+
+        console.log("id of user : --> [", user, "]");
+    } catch (error) {
+        return reply.code(400).send({status: false, message: error.message});
+    }
+    return  reply.code(200).send({status: true, message: 'User registered successfully.'});    
 };
 
 const logout = async (request, reply) => {
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,33 +1,33 @@
 import db from '../db/db.js'
 
 
-const getAllUsers = async () => {
+const getAllUsers =  () => {
     const query = 'SELECT * FROM USERS';
     const users = db.prepare(query)
     return users.all();
 }
 
-const getUserByID = async (id) =>{
+const getUserByID =  (id) => {
     const query = 'SELECT * FROM USERS WHERE id = ?';
     const userID = db.prepare(query);
     return userID.get(id);
 }
 
-const getUserByEmail = async (email) =>{
+const getUserByEmail =  (email) => {
     const query = 'SELECT * FROM USERS WHERE email = ?';
     const userEmail = db.prepare(query);
     return userEmail.get(email);
 }
 
-const getUserByUsername = async (username) =>{
+const getUserByUsername =  (username) => {
     const query = 'SELECT * FROM USERS WHERE username = ?';
     const userUsername = db.prepare(query);
     return userUsername.get(username);
 }
 
-const createUser = async (name, email, password) => {
-    const query = 'INSERT INTO USERS (name, email, password) VALUES (?, ?, ?)';
-    const user = db.prepare(query).run(name, email, password);
+const createUser =  (username, email, password) => {
+    const query = 'INSERT INTO USERS (username, email, password) VALUES (?, ?, ?)';
+    const user = db.prepare(query).run(username, email, password);
     return user.lastInsertRowid;
 }
 
