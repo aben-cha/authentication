86ff28e5fc9f858104948c55cb79fb1ca7fcab2a
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-07-17 12:56:57 +0100

    logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,7 +78,7 @@ const login = async (request, reply) => {
 
 const logout = async (request, reply) => {
     try {
-        reply.clearCookie('token');
+        reply.clearCookie('token', { path: '/api/auth' });
         reply.send({status: true, message: 'Logged out successfully'})
     } catch (error) {
         console.error("logout error:", error);
