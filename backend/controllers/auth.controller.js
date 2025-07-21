3a83c1e044bc5ac17e31d644baec4e2aa54b48dc
Ayoub Ben Chafai <aben-cha@student.1337.ma>
2025-07-21 13:15:49 +0100

    nodemailer:gmail verify-email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -34,7 +34,11 @@ const signup = async (request, reply) => {
             from: `PingPong App ${process.env.EMAIL}`,
             to: email,
             subject: 'Verify your PingPong account',
-            text: VERIFICATION_EMAIL_TEMPLATE(verificationCode)
+            // text: VERIFICATION_EMAIL_TEMPLATE(verificationCode)
+            text: `
+                    <h3>Thank you for registering!</h3>
+                    <h3><a href="${verificationUrl}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none;">Activate your account</a></h3>
+                  `
         };
 
         await transporter.sendMail(mailOptions);
