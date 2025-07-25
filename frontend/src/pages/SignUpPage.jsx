c87ad57e5d65659b5585e3da5ef46ba755111030
aben-cha <aben-cha@student.1337.ma>
2025-07-25 18:09:53 +0200

    Input Component

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion"
 import {Lock, Mail, User} from 'lucide-react'
 import Input from "../components/Input"
 import { useState } from "react"
+import { Link } from 'react-router-dom'
 
 
 const SignUpPage = () => {
@@ -49,7 +50,27 @@ const SignUpPage = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
 
+          <motion.button
+						className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+						font-bold rounded-lg shadow-lg hover:from-green-600
+						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
+						 focus:ring-offset-gray-900 transition duration-200'
+						whileHover={{ scale: 1.02 }}
+						whileTap={{ scale: 0.98 }}
+						type='submit'
+					>
+            Sign Up
+					</motion.button>
+
         </form>
+      </div>
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
       </div>
     </motion.div>
   )
