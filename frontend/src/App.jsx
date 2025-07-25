54bcfd388b0827367edd6f9166460d852cfd9c14
aben-cha <aben-cha@student.1337.ma>
2025-07-25 02:05:22 +0200

    setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
 import FloatingShape from "./components/FloatingShape"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
@@ -16,7 +19,6 @@ function App() {
         <Route path="/signup" element={<SignUpPage />}/>
         <Route path="/login" element={<LoginPage />}/>
       </Routes>
-      
     </div>
   )
 }
