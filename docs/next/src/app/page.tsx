86349ea0026ab2c2fdf5fe9b5cf5019a4532b889
aben-cha <aben-cha@student.1337.ma>
2025-07-29 21:15:38 +0200

    nextjs

--- docs/next/src/app/page.tsx
+++ docs/next/src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <h1>test</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign In
+          </button>
+        </SignInButton>
+        <SignUpButton mode="modal" >
+          <button className="ml-2 bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
     </div>
   );
 }
